a69e5f4c026bad263146d147d0dbaf085356d107
stockworld00-netizen <stockworld00@gmail.com>
2026-02-19 00:20:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_TOKEN = os.getenv("8536725493:AAFSdPtNKJEMFsapJGfH5sh9XtIc-lbruCA")
+TELEGRAM_CHAT_ID = os.getenv("-1003545287392")
 CHECK_INTERVAL = 60  # seconds
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # optional if AI parsing used
